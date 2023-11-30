b3c1298e0c978f5d6d8532b41de78e8d0832ebb8
soapchan <noahfoxgaming@gmail.com>
2023-12-01 10:45:45 +1100

    started making window.py

---
+++ python/game/window.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+class Window:
+	def __init__(self):
+		self.width = 500
+		self.height = 500
+		self.colour = 255, 255, 255
+		self.screen = pygame.display.set_mode((self.width, self.height))
+
+
