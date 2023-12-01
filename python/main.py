2e432852d3173265479f0d38bf7242f6d2ece513
soapchan <noahfoxgaming@gmail.com>
2023-12-01 11:24:31 +1100

    Created bluecube instance of Rect, and drew floor.

--- python/main.py
+++ python/main.py
@@ -4,7 +4,8 @@ import pygame
 
 
 red_cube = Rect(x=100, y=250, width=50, height=50, colour=(255, 0, 0), gravity=0.5, xvel=0, yvel=0, grounded=False)
-floor = Rect(x=0, y=450, width=500, height=50, colour=(0, 0, 0), gravity=0, xvel=0, yvel=0, grounded=True)
+blue_cube = Rect(x=400, y=250, width=50, height=50, colour=(0, 0, 255), gravity=0.5, xvel=0, yvel=0, grounded=False)
+floor = Rect(x=0, y=450, width=500, height=50, colour=(100, 150, 0), gravity=0, xvel=0, yvel=0, grounded=True)
 
 
 class Main:
@@ -20,6 +21,7 @@ while running:
 
 	window.screen.fill(window.colour)
 
+	floor.draw(screen=window.screen)
 	red_cube.draw(screen=window.screen)
 
 	pygame.display.flip()
