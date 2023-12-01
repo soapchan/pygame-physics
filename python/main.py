3e5325a38e31882282d7c9c329eec14264aa00fb
soapchan <noahfoxgaming@gmail.com>
2023-12-01 11:01:17 +1100

    created main.py mainloop

--- python/main.py
+++ python/main.py
@@ -1,9 +1,18 @@
-from game import window
-
-
-window = window.Window
+from game.window import window
+import pygame
 
 
 class Main:
 	def __init__(self):
 		pass
+
+
+running = True
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+	window.screen.fill(window.colour)
+
+	pygame.display.flip()
