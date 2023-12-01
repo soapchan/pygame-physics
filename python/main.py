6fdc73f02070267ab8cf34485c58422b5d427971
soapchan <noahfoxgaming@gmail.com>
2023-12-01 11:22:14 +1100

    Created floor instance of Rect

--- python/main.py
+++ python/main.py
@@ -4,6 +4,7 @@ import pygame
 
 
 red_cube = Rect(x=100, y=250, width=50, height=50, colour=(255, 0, 0), gravity=0.5, xvel=0, yvel=0, grounded=False)
+floor = Rect(x=0, y=450, width=500, height=50, colour=(0, 0, 0), gravity=0, xvel=0, yvel=0, grounded=True)
 
 
 class Main:
