d9b631226c4331390759ac0703e994cf06a9fae7
soapchan <noahfoxgaming@gmail.com>
2023-12-01 11:13:33 +1100

    Started on rect.py and build the draw method.

---
+++ python/objects/rect.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+class Rect:
+	def __init__(self, x, y, width, height, colour, gravity, yvel, xvel, grounded):
+		self.x = x
+		self.y = y
+		self.width = width
+		self.height = height
+		self.colour = colour
+		self.gravity = gravity
+		self.yvel = yvel
+		self.xvel = xvel
+		self.grounded = grounded
+		self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+
+	def draw(self, screen, colour, x, y, width, height):
+		self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+		pygame.draw.rect(screen, colour, (x, y, width, height))
